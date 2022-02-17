d756212b1a4589bedab7e6fff52deeb41095ce3e
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-17 18:40:25 +0100

    using keycloak logout function

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  title = 'client side application';
+  constructor(private route: ActivatedRoute,
+    private router: Router, private keycloakService: KeycloakService) { }
+  ngOnInit() { }
+  logout() {
+    this.keycloakService.logout();
+  }
 }
