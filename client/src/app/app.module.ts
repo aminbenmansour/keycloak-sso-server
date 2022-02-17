89380fd43662fd5c0fe9dfd703a2fae0f009742b
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-17 18:24:07 +0100

    add initialzer and libraries to the root module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
+import { initializer } from '../utils/initializer';
+import { HttpClientModule } from '@angular/common/http';
+
+let keycloakService: KeycloakService = new KeycloakService();
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [ KeycloakService ],
+      multi: true
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
